40b5aebdc8b841b07ec303bb39ceb338a8f03f08
Blaž Suhač <blazsuhac@mini.local>
2025-10-07 18:06:01 +0200

    AuthController now successfully calls Keycloak to obtain access token.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
 {
     public function login(AuthRequest $request)
     {
-        $credentials = $request->only('username', 'password');
-        // Here you would typically validate the credentials and generate a token
-        // For simplicity, we will just return the credentials as JSON
-        return response()->json($credentials);
+        $username = $request->input('username');
+        $password = $request->input('password');
+        $url = 'host.docker.internal:7080/realms/nutrients/protocol/openid-connect/token';
+        $response = Http::asForm()->post($url, [
+            'grant_type' => 'password',
+            'client_id' => 'nutrients-client',
+            'client_secret' => 'PkPLAW1E7x7eQmJMAWa2igKj8RUNlvFy',
+            'username' => $username,
+            'password' => $password,
+            'scope' => 'openid'
+        ]);
+
+        return $response->json();
     }
 }
