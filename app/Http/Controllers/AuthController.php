9878b534a2490af0a4f55e13cb6873355e2f6d2d
Blaž Suhač <blazsuhac@mini.local>
2025-10-08 08:47:04 +0200

    Improvements on AuthController for the login procedure. It now handles keycloak response and returns 401 when login fails. Tests implemented including token cleanup in keycloak after testing.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\AuthRequest;
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthRequest;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
@@ -12,16 +13,31 @@ class AuthController extends Controller
     {
         $username = $request->input('username');
         $password = $request->input('password');
-        $url = 'host.docker.internal:7080/realms/nutrients/protocol/openid-connect/token';
+        $url = config('app.keycloak.base_url') . '/realms/' . config('app.keycloak.realm') . '/protocol/openid-connect/token';
+        
+        // Get token from keycloak
         $response = Http::asForm()->post($url, [
-            'grant_type' => 'password',
-            'client_id' => 'nutrients-client',
-            'client_secret' => 'PkPLAW1E7x7eQmJMAWa2igKj8RUNlvFy',
+            'grant_type' => config('app.keycloak.grant_type'),
+            'client_id' => config('app.keycloak.client_id'),
+            'client_secret' => config('app.keycloak.client_secret'),
             'username' => $username,
             'password' => $password,
-            'scope' => 'openid'
+            'scope' => config('app.keycloak.scope')
         ]);
 
+        if ($response->failed()) {
+            return response()->json($response->json(), $response->status());
+        }
+
+        // Keycloak may return 200 even if the credentials are wrong
+        if (array_key_exists('error', $response->json())) {
+            return response()->json([
+                'error' => $response->json('error'),
+                'error_description' => $response->json('error_description')
+            ], 401);
+        }
+
+        // Return the token response from Keycloak
         return $response->json();
     }
 }
--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,28 +2,22 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AuthenticationTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Test AuthController requests validation
      */
     public function test_login_route(): void
     {
         $username = 'username';
         $password = 'password';
-        $response = $this->postJson(route('login'), [
-            'username' => $username,
-            'password' => $password
-        ]);
-        $response->assertStatus(200, 'Expected HTTP 200 for login, but received ' . $response->status() . '.');
-        $response->assertExactJson([
-            'username' => $username,
-            'password' => $password
-        ], 'The login response JSON does not match the expected credentials.');
 
         // Test username is required
         $response = $this->postJson(route('login'), [
@@ -57,5 +51,103 @@ class AuthenticationTest extends TestCase
 
         $response->assertStatus(422, 'Expected HTTP 422 for validation error, but received ' . $response->status() . '.');
         $response->assertJsonValidationErrors(['password']);
+    }
+
+    /**
+     * Test AuthController login procedure using keycloak
+     */
+    public function test_login_procedure(): void
+    {
+        // Test login using correct credentials
+        $response = $this->postJson(route('login'), [
+            'username' => 'test',
+            'password' => '!2ndArmored'
+        ]);
+        $response->assertStatus(200, 'Expected HTTP 200 for validation error, but received ' . $response->status() . '.');
+        $response->assertJsonStructure([
+            'access_token',
+            'expires_in',
+            'refresh_expires_in',
+            'refresh_token',
+            'token_type',
+            'not-before-policy',
+            'session_state',
+            'scope',
+        ]);
+        $access_token = $response->json('access_token');
+
+        // Test login using incorrect username
+        $response = $this->postJson(route('login'), [
+            'username' => 'incorrect_username',
+            'password' => '!2ndArmored'
+        ]);
+        $response->assertStatus(401, 'Expected HTTP 401 for validation error, but received ' . $response->status() . '.');
+        $response->assertJsonStructure([
+            'error',
+            'error_description'
+        ]);
+
+        // Test login using incorrect password
+        $response = $this->postJson(route('login'), [
+            'username' => 'test',
+            'password' => 'incorrect_password'
+        ]);
+        $response->assertStatus(401, 'Expected HTTP 401 for validation error, but received ' . $response->status() . '.');
+        $response->assertJsonStructure([
+            'error',
+            'error_description',
+        ]);
+
+        // Test login using incorrect client_id
+        $original = config('app.keycloak.client_id');
+        Config::set('app.keycloak.client_id', 'wrong_client_id');
+        $response = $this->postJson(route('login'), [
+            'username' => 'test',
+            'password' => '!2ndArmored'
+        ]);
+        $response->assertStatus(401, 'Expected HTTP 401 for validation error, but received ' . $response->status() . '.');
+        $response->assertJsonStructure([
+            'error',
+            'error_description'
+        ]);
+        Config::set('app.keycloak.client_id', $original);
+
+        // Test login using incorrect client_secret
+        $original = config('app.keycloak.client_secret');
+        Config::set('app.keycloak.client_id', 'wrong_client_secret');
+        $response = $this->postJson(route('login'), [
+            'username' => 'test',
+            'password' => '!2ndArmored'
+        ]);
+        $response->assertJsonStructure([
+            'error',
+            'error_description'
+        ]);
+        Config::set('app.keycloak.client_secret', $original);
+
+        // Test login using incorrect grant_type
+        $original = config('app.keycloak.client_secret');
+        Config::set('app.keycloak.client_id', 'wrong_client_secret');
+        $response = $this->postJson(route('login'), [
+            'username' => 'test',
+            'password' => '!2ndArmored'
+        ]);
+        $response->assertJsonStructure([
+            'error',
+            'error_description'
+        ]);
+
+        Config::set('app.keycloak.client_secret', $original);
+
+        // Clean up keycloak sessions for the user
+        $user = Http::withToken($access_token)->get(config('app.keycloak.base_url') . '//realms/' . config('app.keycloak.realm') . '/protocol/openid-connect/userinfo')->json();
+        $user_id = $user['sub'];
+        $response = Http::asForm()->post(config('app.keycloak.base_url') . '/realms/master/protocol/openid-connect/token', [
+            'grant_type' => 'password',
+            'client_id' => 'admin-cli',
+            'username' => 'admin',
+            'password' => 'admin',
+        ]);
+        $response = Http::withToken($response->json('access_token'))->post(config('app.keycloak.base_url') . "/admin/realms/" . config('app.keycloak.realm') . "/users/{$user_id}/logout");
     }
 }
