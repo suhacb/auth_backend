905a436d3c140b521ed92d55dc8f2620187892a2
Blaž Suhač <blazsuhac@mini.local>
2025-10-25 11:24:16 +0200

    ValidateAccessTokenTest is setUp() to pass the auth. route group middleware (needs to provide application name and url).

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
-
-
 Route::prefix('auth')->name('auth.')->middleware('verify.application')->group(function () {
     Route::post('login', [AuthController::class, 'login'])->name('login');
     Route::post('login-token', [AuthController::class, 'loginToken'])->name('login_token'); // Used to obtain one time login token from application
+    Route::get('validate-access-token', [ApplicationsController::class, 'verifyAccessToken'])->name('validate-access-token');
 });
 
 Route::prefix('applications')->name('applications.')->middleware('verify.auth.application')->group(function () {
--- tests/Feature/ValidateAccessTokenTest.php
+++ tests/Feature/ValidateAccessTokenTest.php
@@ -2,13 +2,32 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Application;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ValidateAccessTokenTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected Application $application;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        // Create a default application
+        $this->application = Application::factory()->create([
+            'name' => 'auth-frontend',
+            'url' => 'https://auth-frontend.example.com',
+        ]);
+ 
+        // Set the default X-Application-Name header
+        $this->defaultHeaders = [
+            'X-Application-Name' => $this->application->name,
+            'X-Client-Url' => $this->application->url
+        ];
+    }
 
     public function test_returns_401_if_there_is_no_access_token (): void
     {
